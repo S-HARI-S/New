934d174860d081e56507ec7448285bb8f878f757
MachoMaheen <kabeerajmal6@gmail.com>
2023-05-06 17:00:39 +0530

    Ui Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
+import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom";
+
+// import RtlLayout from "layouts/rtl";
+import AdminLayout from "@/layouts/admin";
+import AuthLayout from "@/layouts/auth";
 import Update from '@/components/update'
 import logoVite from './assets/logo-vite.svg'
 import logoElectron from './assets/logo-electron.svg'
-import './App.css'
+import '@/assets/css/App.css'
 import SidePanel from './components/Sidepanel'
 import Setup from './components/Setup'
 
@@ -11,15 +16,26 @@ console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}
 
 function App() {
   const [count, setCount] = useState(0)
-  return (
-    <>
-    <div className="flex flex-row">
-    <SidePanel />
-    <Setup />
-    </div>
+  // return (
+  //   <>
+  //   <div className="flex flex-row">
+  //   <SidePanel />
+  //   <Setup />
+  //   </div>
 
-    </>
-  )
+  //   </>
+  // )
+
+  return (
+    
+    <Routes>
+      <Route path="auth/*" element={<AuthLayout />} />
+      <Route path="admin/*" element={<AdminLayout />} />
+      {/* <Route path="rtl/*" element={<RtlLayout />} /> */}
+      <Route path="/" element={<Navigate to="/admin" replace />} />
+    </Routes>
+ 
+  );
 }
 
 export default App
